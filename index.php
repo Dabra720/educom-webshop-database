3a2439b14d350e53b40318083ace5ceb134d510b
Daan Braas <daanbraas@gmail.com>
2023-03-08 18:19:45 +0100

    Issue #8 Bootstrap start

--- index.php
+++ index.php
@@ -192,11 +192,19 @@ function showDocumentStart(){
 }
 
 function showHeadSection($data){
-  echo '<head>
-	        <meta charset="UTF-8">
-	        <title>'. $data['page'] .'</title>
-          <link rel="stylesheet" href="CSS/stylesheet.css">
-        </head>';
+  echo '<head>';
+	echo '<meta charset="UTF-8">';
+	echo'<title>'. $data['page'] .'</title>';
+  echo '<link rel="stylesheet" href="CSS/stylesheet.css">';
+  // echo '<!-- Latest compiled and minified CSS -->
+  // <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">';
+  // echo '<!-- jQuery library -->
+  // <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>';
+  // echo '<!-- Popper JS -->
+  // <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>';
+  // echo '<!-- Latest compiled JavaScript -->
+  // <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>';
+  echo '</head>';
 }
 
 function showHeader($data){
